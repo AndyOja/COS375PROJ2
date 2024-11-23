06f90ad709185720f98cf78d041d0a490e54115f
Anders Oja <ao6499@princeton.edu>
2024-11-23 16:55:25 -0500

    asfd

--- project-2/src/call_graph.cpp
+++ project-2/src/call_graph.cpp
@@ -10,7 +10,7 @@
  */
 
 /*! @file
- *  This file contains an ISA-portable PIN tool for counting dynamic instructions
+ *  This file contains an ISA-portable PIN tool for generating a call graph
  */
 
 #include "pin.H"
@@ -24,11 +24,10 @@ using std::string;
 /* Global Variables */
 /* ===================================================================== */
 
-// COS375 TIP: Add global variables here 
 string routineName;
 bool foundMain = false;
 FILE *outFile;
-int indentLevel = 0;
+int indentLevel = 0; // Track the depth of the call graph
 
 /* ===================================================================== */
 /* Commandline Switches */
@@ -54,69 +53,89 @@ INT32 Usage()
 
 /* ===================================================================== */
 
-VOID docount()
+// Modify docount to accept the argument passed to the function
+void docount(ADDRINT arg0)
 {
-    if (foundMain){
-        //COS375: Add your code here
+    // Print the function argument passed to the call
+    // Output the current function name and the first argument
+    string calleeName = RTN_FindNameByAddress(arg0);
+    if (calleeName == "") {
+        calleeName = "Unknown";  // Default to "Unknown" if function name is not found
     }
+
+    for (int i = 0; i < indentLevel; i++) {
+        fprintf(outFile, " ");  // Indentation based on depth
+    }
+    fprintf(outFile, "%s(%d)\n", calleeName.c_str(), (int)arg0);  // Print function and first argument
 }
 
 /* ===================================================================== */
-// A callback function executed at runtime before executing first
-// instruction in a function
+
+// A callback function executed at runtime before executing first instruction in a function
 void executeBeforeRoutine(ADDRINT ip)
 {
-    // Check if main function is called
-    // If so then set foundMain to true
-    routineName = (RTN_FindNameByAddress(ip));
-    if (routineName.compare("main") == 0){
-        foundMain=true;
-    }    
-    
+    routineName = (RTN_FindNameByAddress(ip));  // Get the routine name
+    if (routineName.compare("main") == 0) {
+        foundMain = true;  // Set foundMain flag when "main" function is encountered
+    }
+
     // Do nothing until main function is seen
-    if (!foundMain){
+    if (!foundMain) {
         return;
     }
-    indentLevel++;
-    //COS375: Add your code here
-    for (int i = 0; i < indentLevel; i++){
-        fprintf(outFile, " ");
-    }
-    fprintf(outFile, "%s(,...)\n", routineName.c_str());
-
+    
     // Check if exit function is called
-    if(routineName.compare("exit") == 0){
-        foundMain=false;
+    if (routineName.compare("exit") == 0) {
+        foundMain = false;  // Reset flag when "exit" is encountered
     }
 }
 
 /* ===================================================================== */
-// Function executed everytime a new routine is found
+
+// This function is executed every time a new routine is found
 VOID Routine(RTN rtn, VOID *v)
 {
     RTN_Open(rtn);
-    //Insert callback to function executeBeforeRoutine which will be 
-    //executed just before executing first instruction in the routine
-    //at runtime
+    
+    // Insert callback to function executeBeforeRoutine which will be executed just before executing first instruction in the routine at runtime
     INS_InsertCall(RTN_InsHead(rtn), IPOINT_BEFORE, (AFUNPTR)executeBeforeRoutine, IARG_INST_PTR, IARG_END);
 
-    //Iterate over all instructions of routne rtn
-    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)){
-        //COS375: Add your code here
-        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_END);
+    // Iterate over all instructions in the routine
+    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
+        // Check if the instruction is a function call (using PIN API)
+        if (INS_IsCall(ins)) {
+            // Insert callback to log function call information
+            // Capture the first argument of the function
+            ADDRINT arg0 = 0;
+            if (INS_OperandCount(ins) > 0) {
+                INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount,
+                               IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_END);  // Get the first argument
+            }
+
+            // Get the name of the called function
+            string calleeName = RTN_FindNameByAddress(INS_Address(ins));
+            if (calleeName == "") {
+                calleeName = "Unknown";  // Default to "Unknown" if the function name is not found
+            }
+
+            // Print the function call with indentation based on the depth
+            for (int i = 0; i < indentLevel; i++) {
+                fprintf(outFile, " ");  // Indentation based on depth
+            }
+            fprintf(outFile, "%s(%d)\n", calleeName.c_str(), (int)arg0);  // Print function and first argument
+        }
     }
+
     RTN_Close(rtn);
 }
 
 /* ===================================================================== */
-// Function executed after instrumentation
+
+// Function executed after instrumentation to dump the results
 VOID Fini(INT32 code, VOID *v)
 {
-    //COS375: Add your code here to dump instrumentation data that is collected.
-    fprintf(outFile,"COS375 pin tool Template");
-    fclose(outFile);
+    fclose(outFile);  // Close the output file
 }
-
 
 // DO NOT EDIT CODE AFTER THIS LINE
 /* ===================================================================== */
@@ -126,17 +145,18 @@ VOID Fini(INT32 code, VOID *v)
 int main(int argc, char *argv[])
 {
     PIN_InitSymbols();
-    if( PIN_Init(argc,argv) )
-    {
+    if (PIN_Init(argc, argv)) {
         return Usage();
     }
-    
 
-    outFile = fopen("call_graph.out","w");
+    // Open the output file for the call graph
+    outFile = fopen("call_graph.out", "w");
+
+    // Add instrumentation function for every routine
     RTN_AddInstrumentFunction(Routine, 0);
     PIN_AddFiniFunction(Fini, 0);
 
-    // Never returns
+    // Start the program execution
     PIN_StartProgram();
     
     return 0;
