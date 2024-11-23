da2c2ab87da437ccfcdcf2b8771b9539a53ce0e8
Anders Oja <ao6499@princeton.edu>
2024-11-23 16:15:28 -0500

    asfd

--- project-2/src/call_graph.cpp
+++ project-2/src/call_graph.cpp
@@ -1,29 +1,38 @@
+/*
+ * Copyright 2002-2020 Intel Corporation.
+ * 
+ * This software is provided to you as Sample Source Code as defined in the accompanying
+ * End User License Agreement for the Intel(R) Software Development Products ("Agreement")
+ * section 1.L.
+ * 
+ * This software and the related documents are provided as is, with no express or implied
+ * warranties, other than those that are expressly stated in the License.
+ */
+
+/*! @file
+ *  This file contains an ISA-portable PIN tool for generating a call graph
+ */
+
 #include "pin.H"
 #include <iostream>
 #include <string.h>
-#include <vector>
 using std::cerr;
 using std::endl;
 using std::string;
-using std::vector;
 
 /* ===================================================================== */
 /* Global Variables */
 /* ===================================================================== */
 
-
 string routineName;
 bool foundMain = false;
 FILE *outFile;
-// Global variables to track function calls and their depths
-int depthLevel = 0; // Tracks the depth of function calls (indentation level)
-vector<string> funcNames; // Stores the names of functions
-vector<int> funcDepths; // Stores the depth of each function call
-vector<ADDRINT> funcArg; // Stores the first argument for each function call
+int indentLevel = 0; // Track the depth of the call graph
 
 /* ===================================================================== */
 /* Commandline Switches */
 /* ===================================================================== */
+
 
 /* ===================================================================== */
 /* Print Help Message                                                    */
@@ -31,38 +40,32 @@ vector<ADDRINT> funcArg; // Stores the first argument for each function call
 
 INT32 Usage()
 {
-    cerr << "This tool is the template for all instrumentations in project-2.\n" << endl;
+    cerr <<
+        "This tool is the template for all instrumentations in project-2.\n"
+        "\n";
+
     cerr << KNOB_BASE::StringKnobSummary();
+
     cerr << endl;
+
     return -1;
 }
 
 /* ===================================================================== */
 
-// Callback function executed at runtime for every instruction
-VOID docount(ADDRINT arg0)
+VOID docount()
 {
-    // We track the function call information only when depth is greater than 0
-    if (depthLevel > 0) {
-        funcNames.push_back(routineName);
-        funcDepths.push_back(depthLevel);
-        
-        // Assuming the first argument is being captured (IARG_FUNCARG_ENTRYPOINT_VALUE, 0)
-        funcArg.push_back(arg0);
-    }
+    // This is a placeholder to maintain your structure. The real work is done in executeBeforeRoutine
 }
 
 /* ===================================================================== */
 
-// Callback function executed at runtime before executing first
-// instruction in a function
+// A callback function executed at runtime before executing first instruction in a function
 void executeBeforeRoutine(ADDRINT ip)
 {
-    routineName = RTN_FindNameByAddress(ip);
-    
-    // If the routine name is "main", start tracking
+    routineName = (RTN_FindNameByAddress(ip));  // Get the routine name
     if (routineName.compare("main") == 0) {
-        foundMain = true;
+        foundMain = true;  // Set foundMain flag when "main" function is encountered
     }
 
     // Do nothing until main function is seen
@@ -70,72 +73,66 @@ void executeBeforeRoutine(ADDRINT ip)
         return;
     }
     
-    // Handle the return of a function
+    // Check if exit function is called
     if (routineName.compare("exit") == 0) {
-        foundMain = false;
+        foundMain = false;  // Reset flag when "exit" is encountered
     }
 }
 
 /* ===================================================================== */
 
-// Function executed every time a new routine is found
+// This function is executed every time a new routine is found
 VOID Routine(RTN rtn, VOID *v)
 {
     RTN_Open(rtn);
     
-    // Insert the callback to capture the first instruction in the routine
+    // Insert callback to function executeBeforeRoutine which will be executed just before executing first instruction in the routine at runtime
     INS_InsertCall(RTN_InsHead(rtn), IPOINT_BEFORE, (AFUNPTR)executeBeforeRoutine, IARG_INST_PTR, IARG_END);
 
-    // Iterate over all instructions of the current routine
+    // Iterate over all instructions in the routine
     for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
-        // For function call instructions, we increment depth first
+        // Check if the instruction is a function call (using PIN API)
         if (INS_IsCall(ins)) {
-            // Insert the callback for the current instruction (docount)
-            INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount,
-            IARG_FUNCARG_ENTRYPOINT_VALUE, 0, IARG_END);
+            // Insert callback to log function call information
+            // Capture the address of the function being called and the first argument passed
+            INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_END);
 
-            // Increment the depth after inserting the callback for the call instruction
-            depthLevel++;
-        }
-        // For function return instructions, we decrement depth first
-        else if (INS_IsRet(ins)) {
+            // Get the name of the called function
+            string calleeName = RTN_FindNameByAddress(INS_Address(ins));
+            if (calleeName == "") {
+                calleeName = "Unknown";  // Default to "Unknown" if the function name is not found
+            }
 
-            // Decrement the depth before executing the return instruction
-            depthLevel--;
+            // Capture the first argument of the function
+            ADDRINT arg0 = 0;
+            if (INS_OperandCount(ins) > 0) {
+                arg0 = INS_OperandValue(ins, 0);
+            }
+
+            // Print the function call with indentation based on the depth
+            for (int i = 0; i < indentLevel; i++) {
+                fprintf(outFile, " ");  // Indentation based on depth
+            }
+            fprintf(outFile, "%s(%d)\n", calleeName.c_str(), (int)arg0);  // Print function and first argument
         }
     }
-    
+
     RTN_Close(rtn);
 }
 
 /* ===================================================================== */
 
-// Function executed after instrumentation
+// Function executed after instrumentation to dump the results
 VOID Fini(INT32 code, VOID *v)
 {
-    // Print the collected data in the correct format
-    FILE *outFile = fopen("call_graph.out", "w");
-    if (outFile != NULL) {
-        for (size_t i = 0; i < funcNames.size(); ++i) {
-            // Print the function name with the correct indentation (based on depth)
-            for (int j = 0; j < funcDepths[i]; ++j) {
-                fprintf(outFile, " ");
-            }
-            fprintf(outFile, "%s", funcNames[i].c_str());
-
-            // If a function has an argument, print the first argument
-            if (funcArg.size() > i) {
-                fprintf(outFile, " %p", (void*)funcArg[i]);
-            }
-            fprintf(outFile, "\n");
-        }
-        fclose(outFile);
-    }
+    fclose(outFile);  // Close the output file
 }
 
+// DO NOT EDIT CODE AFTER THIS LINE
+/* ===================================================================== */
+/* Main                                                                  */
 /* ===================================================================== */
 
-// Main function
 int main(int argc, char *argv[])
 {
     PIN_InitSymbols();
@@ -143,6 +140,10 @@ int main(int argc, char *argv[])
         return Usage();
     }
 
+    // Open the output file for the call graph
+    outFile = fopen("call_graph.out", "w");
+
+    // Add instrumentation function for every routine
     RTN_AddInstrumentFunction(Routine, 0);
     PIN_AddFiniFunction(Fini, 0);
 
@@ -154,3 +155,4 @@ int main(int argc, char *argv[])
 
 /* ===================================================================== */
 /* eof */
+/* ===================================================================== */
