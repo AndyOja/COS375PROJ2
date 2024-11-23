f22a639b320cd126f5fe7053f18b7b3c215de7d9
Anders Oja <ao6499@princeton.edu>
2024-11-23 15:34:13 -0500

    akfjdsf

--- project-2/src/call_graph.cpp
+++ project-2/src/call_graph.cpp
@@ -1,38 +1,25 @@
-/*
- * Copyright 2002-2020 Intel Corporation.
- * 
- * This software is provided to you as Sample Source Code as defined in the accompanying
- * End User License Agreement for the Intel(R) Software Development Products ("Agreement")
- * section 1.L.
- * 
- * This software and the related documents are provided as is, with no express or implied
- * warranties, other than those that are expressly stated in the License.
- */
-
-/*! @file
- *  This file contains an ISA-portable PIN tool for counting dynamic instructions
- */
-
 #include "pin.H"
 #include <iostream>
 #include <string.h>
+#include <vector>
 using std::cerr;
 using std::endl;
 using std::string;
+using std::vector;
 
 /* ===================================================================== */
 /* Global Variables */
 /* ===================================================================== */
 
-// COS375 TIP: Add global variables here 
-string routineName;
-bool foundMain = false;
-FILE *outFile;
+// Global variables to track function calls and their depths
+int depthLevel = 0; // Tracks the depth of function calls (indentation level)
+vector<string> funcNames; // Stores the names of functions
+vector<int> funcDepths; // Stores the depth of each function call
+vector<ADDRINT> funcArg; // Stores the first argument for each function call
 
 /* ===================================================================== */
 /* Commandline Switches */
 /* ===================================================================== */
-
 
 /* ===================================================================== */
 /* Print Help Message                                                    */
@@ -40,98 +27,123 @@ FILE *outFile;
 
 INT32 Usage()
 {
-    cerr <<
-        "This tool is the template for all instrumentations in project-2.\n"
-        "\n";
-
+    cerr << "This tool is the template for all instrumentations in project-2.\n" << endl;
     cerr << KNOB_BASE::StringKnobSummary();
-
     cerr << endl;
-
     return -1;
 }
 
 /* ===================================================================== */
 
+// Callback function executed at runtime for every instruction
 VOID docount()
 {
-    if (foundMain){
-        //COS375: Add your code here
+    // We track the function call information only when depth is greater than 0
+    if (depthLevel > 0) {
+        funcNames.push_back(routineName);
+        funcDepths.push_back(depthLevel);
+        
+        // Assuming the first argument is being captured (IARG_FUNCARG_ENTRYPOINT_VALUE, 0)
+        funcArg.push_back(funcArgVal);
     }
 }
 
 /* ===================================================================== */
-// A callback function executed at runtime before executing first
+
+// Callback function executed at runtime before executing first
 // instruction in a function
 void executeBeforeRoutine(ADDRINT ip)
 {
-    // Check if main function is called
-    // If so then set foundMain to true
-    routineName = (RTN_FindNameByAddress(ip));
-    if (routineName.compare("main") == 0){
-        foundMain=true;
-    }    
+    routineName = RTN_FindNameByAddress(ip);
     
-    // Do nothing until main function is seen
-    if (!foundMain){
-        return;
+    // If the routine name is "main", start tracking
+    if (routineName.compare("main") == 0) {
+        foundMain = true;
     }
 
-    //COS375: Add your code here
-        
-    // Check if exit function is called
-    if(routineName.compare("exit") == 0){
-        foundMain=false;
+    // Do nothing until main function is seen
+    if (!foundMain) {
+        return;
+    }
+    
+    // Handle the return of a function
+    if (routineName.compare("exit") == 0) {
+        foundMain = false;
     }
 }
 
 /* ===================================================================== */
-// Function executed everytime a new routine is found
+
+// Function executed every time a new routine is found
 VOID Routine(RTN rtn, VOID *v)
 {
     RTN_Open(rtn);
-    //Insert callback to function executeBeforeRoutine which will be 
-    //executed just before executing first instruction in the routine
-    //at runtime
+    
+    // Insert the callback to capture the first instruction in the routine
     INS_InsertCall(RTN_InsHead(rtn), IPOINT_BEFORE, (AFUNPTR)executeBeforeRoutine, IARG_INST_PTR, IARG_END);
 
-    //Iterate over all instructions of routne rtn
-    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)){
-        //COS375: Add your code here
-        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_END);
+    // Iterate over all instructions of the current routine
+    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
+        // For function call instructions, we increment depth first
+        if (INS_IsCall(ins)) {
+            // Insert the callback for the current instruction (docount)
+            INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_END);
+
+            // Increment the depth after inserting the callback for the call instruction
+            depthLevel++;
+        }
+        // For function return instructions, we decrement depth first
+        else if (INS_IsRet(ins)) {
+            // Insert the callback for the current instruction (docount)
+            INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_END);
+
+            // Decrement the depth before executing the return instruction
+            depthLevel--;
+        }
     }
+    
     RTN_Close(rtn);
 }
 
 /* ===================================================================== */
+
 // Function executed after instrumentation
 VOID Fini(INT32 code, VOID *v)
 {
-    //COS375: Add your code here to dump instrumentation data that is collected.
-    fprintf(outFile,"COS375 pin tool Template");
-    fclose(outFile);
+    // Print the collected data in the correct format
+    FILE *outFile = fopen("call_graph.out", "w");
+    if (outFile != NULL) {
+        for (size_t i = 0; i < funcNames.size(); ++i) {
+            // Print the function name with the correct indentation (based on depth)
+            for (int j = 0; j < funcDepths[i]; ++j) {
+                fprintf(outFile, " ");
+            }
+            fprintf(outFile, "%s", funcNames[i].c_str());
+
+            // If a function has an argument, print the first argument
+            if (funcArg.size() > i) {
+                fprintf(outFile, " %p", (void*)funcArg[i]);
+            }
+            fprintf(outFile, "\n");
+        }
+        fclose(outFile);
+    }
 }
 
-
-// DO NOT EDIT CODE AFTER THIS LINE
-/* ===================================================================== */
-/* Main                                                                  */
 /* ===================================================================== */
 
+// Main function
 int main(int argc, char *argv[])
 {
     PIN_InitSymbols();
-    if( PIN_Init(argc,argv) )
-    {
+    if (PIN_Init(argc, argv)) {
         return Usage();
     }
-    
 
-    outFile = fopen("call_graph.out","w");
     RTN_AddInstrumentFunction(Routine, 0);
     PIN_AddFiniFunction(Fini, 0);
 
-    // Never returns
+    // Start the program execution
     PIN_StartProgram();
     
     return 0;
@@ -139,4 +151,3 @@ int main(int argc, char *argv[])
 
 /* ===================================================================== */
 /* eof */
-/* ===================================================================== */
